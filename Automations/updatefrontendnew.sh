1e506929bd142e23b08fde5bb762174ee2f95a09
Pratik Mahara <maharapratik5@gmail.com>
2026-01-09 11:50:51 +0530

    Update frontend script to use localhost for VITE_API_PATH
    Change the method of retrieving the IP address to use localhost and update the .env file accordingly.

--- Automations/updatefrontendnew.sh
+++ Automations/updatefrontendnew.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
-# Set the Instance ID and path to the .env file
-INSTANCE_ID="i-030da7d31a1dbbffc"
+# Use localhost (or your LAN IP if needed)
+LOCAL_IP="localhost"
 
-# Retrieve the public IP address of the specified EC2 instance
-ipv4_address=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
-
-# Path to the .env file
+# Path to the frontend .env file
 file_to_find="../frontend/.env.docker"
 
-# Check the current VITE_API_PATH in the .env file
-current_url=$(cat $file_to_find)
+# Desired VITE_API_PATH
+new_url="VITE_API_PATH=\"http://${LOCAL_IP}:31100\""
 
-# Update the .env file if the IP address has changed
-if [[ "$current_url" != "VITE_API_PATH=\"http://${ipv4_address}:31100\"" ]]; then
+# Check the current VITE_API_PATH in the .env file
+current_url=$(grep "VITE_API_PATH" $file_to_find)
+
+# Update the .env file if the URL has changed
+if [[ "$current_url" != "$new_url" ]]; then
     if [ -f $file_to_find ]; then
-        sed -i -e "s|VITE_API_PATH.*|VITE_API_PATH=\"http://${ipv4_address}:31100\"|g" $file_to_find
+        sed -i -e "s|VITE_API_PATH.*|$new_url|g" $file_to_find
+        echo "Updated VITE_API_PATH to $new_url"
     else
-        echo "ERROR: File not found."
+        echo "ERROR: File not found at $file_to_find"
     fi
+else
+    echo "VITE_API_PATH is already up-to-date"
 fi
+
